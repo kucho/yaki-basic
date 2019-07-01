7e248b1b9f152110c84883f343a23a71adba8887
Víctor Rodríguez <victor.rodriguez.guriz@gmail.com>
2019-06-30 19:24:30 -0500

    Fixied client data in order

--- js/login.js
+++ js/login.js
@@ -1,19 +1,18 @@
-function validar() {
+function validar () {
+  let usuario = document.getElementById('user').value
+  let clave = document.getElementById('pass').value
 
-	let usuario = document.getElementById("user").value
-	let clave = document.getElementById("pass").value
-	const formulario = document.getElementById("login-form")
-
-	if (usuario == "demo") {
-		if (clave == "1234") {
-			// acá redirecciona
-			formulario.action = "./reserva.html"
-		} else {
-			// avisamos que la clave está mal
-			alert("La clave no es correcta")
-		}
-	} else {
-		// avisamos que el usuario está mal
-		alert("El usuario no es correcto")
-	}
+  if (usuario == 'demo') {
+    if (clave == '1234') {
+      // acá redirecciona
+      window.location.assign('./reserva.html')
+    } else {
+      // avisamos que la clave está mal
+      alert('La clave no es correcta')
+    }
+  } else {
+    // avisamos que el usuario está mal
+    alert('El usuario no es correcto')
+  }
+  return false
 }
